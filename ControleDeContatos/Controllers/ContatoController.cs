96b80ecf5ef2e1864f45263f5f7849865a660dd8
Dantom Nunes <dantom.nunes@hotmail.com>
2024-02-14 21:23:01 -0300

    Criação das views: criar, editar e apagar contato

---
+++ ControleDeContatos/Controllers/ContatoController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ControleDeContatos.Controllers
+{
+    public class ContatoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult CriarContato()
+        {
+            return View();
+        }
+
+        public IActionResult EditarContato()
+        {
+            return View();
+        }
+
+        public IActionResult ApagarContato()
+        {
+            return View();
+        }
+    }
+}
