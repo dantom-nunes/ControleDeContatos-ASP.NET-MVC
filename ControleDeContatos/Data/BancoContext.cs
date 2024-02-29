2dc047ce7f15dba79611315fd77b9a01bde0b8aa
Dantom Nunes <dantom.nunes@hotmail.com>
2024-02-29 11:30:51 -0300

    Criando tela de login usuário

--- ControleDeContatos/Data/BancoContext.cs
+++ ControleDeContatos/Data/BancoContext.cs
@@ -9,5 +9,6 @@ namespace ControleDeContatos.Data
         public BancoContext(DbContextOptions<BancoContext> options) : base(options) { }
 
         public DbSet<ContatoModel> Contatos { get; set; }
+        public DbSet<UsuarioModel> Usuarios { get; set; }
     }
 }
---
+++ ControleDeContatos/Models/UsuarioModel.cs
@@ -0,0 +1,16 @@
+﻿using ControleDeContatos.Enums;
+
+namespace ControleDeContatos.Models
+{
+    public class UsuarioModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Login{ get; set;}
+        public string Email { get; set;}
+        public string Senha { get; set; }
+        public PerfilEnum Perfil { get; set;}
+        public DateTime DataCadastro { get; set;}
+        public DateTime? DataAtualizacao { get; set; }
+    }
+}
